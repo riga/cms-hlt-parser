ee85693a0f3bac7c3313e2c9cfed44143489663e
Marcel R <github.riga@icloud.com>
2023-08-18 20:03:20 +0200

    Fix interplay between bril and outer python envs.

--- sandboxes/env_bril.sh
+++ sandboxes/env_bril.sh
@@ -7,7 +7,7 @@ action() {
     # adjust local software paths
     local bril_dir="${HLTP_SOFTWARE}/bril"
     local pyv="$( python -c "import sys; print('{0.major}.{0.minor}'.format(sys.version_info))" )"
-    export PYTHONPATH="${bril_dir}/lib/python${pyv}/site-packages:${PYTHONPATH}"
+    export PYTHONPATH="${bril_dir}/lib/python${pyv}/site-packages:/cvmfs/cms-bril.cern.ch/brilconda3/lib/python${pyv}/site-packages:${PYTHONPATH}"
     export PATH="${bril_dir}/bin:${PATH}"
 
     if ! type brilcalc &> /dev/null; then
